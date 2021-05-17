a5b6b62bdf448b625a1d3227994f5eb5f0604007
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-05-17 11:31:06 +0200

    feat(db): add pg client (#9)

---
+++ src/playground/db.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { Pool } = require('pg');
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+  user: 'gitpod',
+});
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res.rows);
+  pool.end();
+});
