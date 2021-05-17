bbf985add490fce4cf7cedbcfeeaa706d37e3ba7
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-05-17 14:59:23 +0000

    feat(users): add local server and client Fixes #24

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlaceholderModule } from './placeholder/placeholder.module';
 
 @Module({
-  imports: [],
+  imports: [PlaceholderModule],
   controllers: [AppController],
   providers: [AppService],
 })
